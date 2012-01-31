2da205e2476f09a15e727fbfaf1015c645719f9c
Tom Myers <tom.stephen.myers@gmail.com>
2012-01-31 16:29:29 +0000

    added a processing function with a simple canny edge detection example.

--- phase.py
+++ phase.py
@@ -1,24 +1,45 @@
-import cv, convert
+import cv
 import numpy as np
+from convert import *
 
 # Use webcam for input if we can make the algorithm run in real time?
-CAM_OR_LENA = 'lena'
+CAM_OR_LENA = 'cam'
 
 cv.NamedWindow('Input')
 cv.NamedWindow('Output')
 
+def processing(input):
+	'''DO ALL PROCESSING IN HERE...'''
+	
+	# Convert to greyscale
+	grey = cv.CreateImage((input.width, input.height), 8, 1)
+	edges = cv.CloneImage(grey)
+	cv.CvtColor(input, grey, cv.CV_RGB2GRAY)
+	
+	# Simple Canny edge detection
+	cv.Canny(grey, edges, 70, 100)
+	 
+	# Convert to numpy array
+	array = cv2array(grey)
+	
+	output = edges
+	return output
+	
+
 if CAM_OR_LENA == 'lena':
+	print 'Press any key to quit..'
+	
 	lena = cv.LoadImage('lena.jpg')
 	cv.ShowImage('Input', lena)
-
-	# Do processing here...
-	array = convert.cv2array(lena)
-
-	cv.ShowImage('Output', lena)
+	
+	output = processing(lena)
+	cv.ShowImage('Output', output)
 	
 	cv.WaitKey(0)
 	
 else:
+	print 'Press Esc to quit...'
+	
 	cam = cv.CreateCameraCapture(0)
 
 	while 1:
@@ -30,11 +51,11 @@ else:
 		cv.Flip(frame, None, 1)
 		cv.ShowImage('Input', frame)
 	
-		# Do processing here...
-		cv.ShowImage('Output', frame)
+		output = processing(frame)
+		cv.ShowImage('Output', output)
 	
 		# Handle events
 		k = cv.WaitKey(5)
 
-		if k == 0x1b or k == 0x51 or k == 0x71: # Esc/Q/q to quit
+		if k == 0x1b: # Esc to quit
 		    break
