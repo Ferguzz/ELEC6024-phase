Finished implementation, however there is still something wrong and it does not currently run.  Bug is towards the end, perhaps in noise threshold calculation.  Also split up file structure in order to have a byte code version of phase.py for faster startup.  If phase.py is run alone, it loads a pickled bumpy array representing the kitten image.

phase.py
@@ -1,67 +1,63 @@
-import cv
-import numpy as np
-from convert import *
+from numpy import *
 
-# Use webcam for input if we can make the algorithm run in real time?
-CAM_OR_LENA = 'lena'
-
-def phaseconv(input, nscale = 4, norient = 6, minWaveLength = 3, 
+def phasecong(input, nscale = 4, norient = 6, minWaveLength = 3, 
 			  mult = 2.1, sigmaOnf = 0.55, k = 2.0, cutOff = 0.5, 
 			  g = 10, noiseMethod = -1):
-
+				
 	epsilon = 0.0001
 	rows, cols = input.shape
-	imagefft = np.fft.fft2(input)
+	imagefft = fft.fft2(input)
 
-	zero = np.zeros((rows,cols))
-	E0 = np.zeros((nscale, norient), dtype = np.object)
-	PC = np.zeros((norient,1), dtype = np.object)
+	zero = zeros((rows,cols))
+	EO = zeros((nscale, norient), dtype = object)
+	PC = zeros((norient,1), dtype = object)
 	covx2 = zero
 	covy2 = zero
 	covxy = zero
 	
-	EnergyV = np.zeros((rows,cols,3))
-	pcSum = np.zeros((rows,cols))
+	EnergyV = zeros((3, rows,cols))
+	pcSum = zeros((rows,cols))
 	
 	if cols%2:
-		rangex = np.arange(-(cols-1)/2, (cols-1)/2 + 1 )/float(cols-1)
+		rangex = arange(-(cols-1)/2, (cols-1)/2 + 1 )/float(cols-1)
 	else:
-		rangex = np.arange(-cols/2, cols/2)/float(cols)
+		rangex = arange(-cols/2, cols/2)/float(cols)
 	
 	if rows%2:
-		rangey = np.arange(-(rows-1)/2, (rows-1)/2 + 1)/float(rows-1)
+		rangey = arange(-(rows-1)/2, (rows-1)/2 + 1)/float(rows-1)
 	else:
-		rangey = np.arange(-rows/2, rows/2)/float(rows)
+		rangey = arange(-rows/2, rows/2)/float(rows)
 
 		
-	x,y = np.meshgrid(rangex, rangey)
-	radius = np.sqrt(x**2 + y**2)
-	theta = np.arctan2(-y, x)
-	radius = np.fft.ifftshift(radius)
-	theta = np.fft.ifftshift(theta)
-	sintheta = np.sin(theta)
-	costheta = np.cos(theta)
+	x,y = meshgrid(rangex, rangey)
+	radius = sqrt(x**2 + y**2)
+	theta = arctan2(-y, x)
+	radius = fft.ifftshift(radius)
+	theta = fft.ifftshift(theta)
+	sintheta = sin(theta)
+	costheta = cos(theta)
 	
-	lp = np.fft.ifftshift(1.0 / (1.0 + (radius / 0.45)**(2*15)))
+	lp = fft.ifftshift(1.0 / (1.0 + (radius / 0.45)**(2*15)))
 	radius[0,0] = 1
 	
-	logGabor = np.zeros(nscale, dtype = np.object)
+	logGabor = zeros(nscale, dtype = object)
 	
 	for s in range(nscale):
 		wavelength = minWaveLength*(mult**s)
 		fo = 1.0/wavelength
-		logGabor[s] = np.exp((-(np.log(radius/fo))**2) / (2* np.log(sigmaOnf)**2))
+		logGabor[s] = exp((-(log(radius/fo))**2) / (2* log(sigmaOnf)**2))
 		logGabor[s] = logGabor[s]*lp
 		logGabor[s][0,0] = 0
-	
+
+	# Main loop ...
 	for o in range(norient):
-		angl = (o)*np.pi/norient
-		ds = sintheta * np.cos(angl) - costheta * np.sin(angl)
-		dc = costheta * np.cos(angl) - sintheta * np.cos(angl)
-		dtheta = np.abs(np.arctan2(ds,dc))
-		dtheta = np.clip(dtheta*norient/2, -np.inf, np.pi)
-		spread = (np.cos(dtheta) + 1)/2
-		
+		angl = (o)*pi/norient
+		ds = sintheta * cos(angl) - costheta * sin(angl)
+		dc = costheta * cos(angl) - sintheta * sin(angl)
+		dtheta = abs(arctan2(ds,dc))
+		dtheta = clip(dtheta*norient/2, -inf, pi)
+		spread = (cos(dtheta) + 1)/2
+						
 		sumE_ThisOrient = zero
 		sumO_ThisOrient = zero
 		sumAn_ThisOrient = zero
@@ -69,72 +65,80 @@ def phaseconv(input, nscale = 4, norient = 6, minWaveLength = 3,
 		
 		for s in range(nscale):
 			filter = logGabor[s]*spread
-			E0[s,o] = np.fft.ifft2(imagefft * filter)
-			An = np.abs(E0[s,o])
+			EO[s,o] = fft.ifft2(imagefft * filter)
+			An = abs(EO[s,o])
 			
 			sumAn_ThisOrient = sumAn_ThisOrient + An
-			sumE_ThisOrient = sumE_ThisOrient + np.real(E0[s,o])
-			sumO_ThisOrient = sumO_ThisOrient + np.imag(E0[s,o])
-			
+			sumE_ThisOrient = sumE_ThisOrient + real(EO[s,o])
+			sumO_ThisOrient = sumO_ThisOrient + imag(EO[s,o])
 			if s == 0:
 				if noiseMethod == -1:
-					tau = np.median(sumAn_ThisOrient)
-					print tau
-			exit()
+					tau = median(sumAn_ThisOrient/sqrt(log(4)))
+				elif noiseMethod == -2:
+					nbins = 50
+					mx = amax(sumAn_ThisOrient)
+					edges = arange(0, mx, mx/nbins)
+					hist = histogram(sumAn_ThisOrient, edges)
+					n = bincount(hist[0])
+					dum = amax(n)
+					ind = argmax(n)
+					tau = (edges[ind] + edges[ind+1])/2
+					print 'Don\'t use this mode yet.  Not 100%% sure it\'s working as it should.'
+				maxAn = An
+			else:
+				maximum(maxAn, An)
+			
+		EnergyV[0] = EnergyV[0] + sumE_ThisOrient
+		EnergyV[1] = EnergyV[1] + cos(angl)*sumO_ThisOrient
+		EnergyV[2] = EnergyV[2] + sin(angl)*sumO_ThisOrient	
 		
-def processing(input):
-	'''DO ALL PROCESSING IN HERE...'''
+		XEnergy = sqrt(sumE_ThisOrient**2 + sumO_ThisOrient**2) + epsilon
+		MeanE = sumE_ThisOrient / XEnergy
+		MeanO = sumO_ThisOrient / XEnergy
+		
+		for s in range(nscale):
+			E = real(EO[s,o])
+			O = imag(EO[s,o])
+			Energy = Energy + E*MeanE + O*MeanO - abs(E*MeanO - O*MeanE)
+				
+		if noiseMethod >= 0:
+			T = noiseMethod
+		else:
+			totalTau = tau*(1 - (1/mult)**nscale)/(1-(1/mult))
+			EstNoiseEnergyMean = totalTau*sqrt(pi/2)
+			EstNoiseEnergySigma = totalTau*sqrt((4-pi)/2)
+			T = EstNoiseEnergyMean + k*EstNoiseEnergySigma
+			
+		Energy = maximum(Energy - T, 0)
+		width = (sumAn_ThisOrient/(maxAn + epsilon) - 1) / (nscale - 1)
+		weight = 1.0 / (1 + exp((cutOff-width)*g))
+		
+		PC[o] = weight*Energy/sumAn_ThisOrient
+		pcSum = pcSum+PC[o]
+		
+		covx = PC[o]*cos(angl)
+		covy = PC[o]*sin(angl)
+		covx2 = covx2 + covx**2
+		covy2 = covy2 + covy**2
+		covxy = covxy + covx*covy
 	
-	# Convert to greyscale
-	grey = cv.CreateMat(input.width, input.height, cv.CV_8UC1)
-	edges = cv.CreateMat(input.width, input.height, cv.CV_8UC1)
-
-	cv.CvtColor(input, grey, cv.CV_RGB2GRAY)
+	covx2 = covx2/(norient/2)
+	covy2 = covy2/(norient/2)
+	covxy = 4*covxy/norient;
+	denom = sqrt(covxyv**2 + (covx2 - covy2)**2) + epsilon
+	M = (covy2+covx2 + denom)/2
+	m = (covy2+covx2 - denom)/2
 	
-	# Simple Canny edge detection
-	cv.Canny(grey, edges, 70, 100)
+	Or = arctan2(EnergyV[2], EnergyV[1])
+	Or[Or<0] = Or[Or<0] + pi
+	Or = round(Or*180/pi)
 	
-	# Convert image to numpy array	
-	im = np.asarray(grey)
-	phaseconv(im)
+	OddV = sqrt(EnergyV[1]**2 + EnergyV[2]**2)
+	featType = arctan2(EnergyV[0], OddV)
 	
-	output = edges
-	return output
-
 if __name__ == '__main__':
-
-	cv.NamedWindow('Input')
-	cv.NamedWindow('Output')	
-
-	if CAM_OR_LENA == 'lena':
-		print 'Press any key to quit..'
-	
-		lena = cv.LoadImageM('lena.jpg', cv.CV_LOAD_IMAGE_UNCHANGED)
-		cv.ShowImage('Input', lena)
- 		output = processing(lena)
-		cv.ShowImage('Output', output)
-	
-		cv.WaitKey(0)
-	
-	else:
-		print 'Press Esc to quit...'
-	
-		cam = cv.CreateCameraCapture(0)
-
-		while 1:
-			frame = cv.QueryFrame(cam)
-			if frame == None:
-				'Dropped frame...'
-				continue
-		
-			cv.Flip(frame, None, 1)
-			cv.ShowImage('Input', frame)
-	
-			output = processing(frame)
-			cv.ShowImage('Output', output)
-	
-			# Handle events
-			k = cv.WaitKey(5)
-
-			if k == 0x1b: # Esc to quit
-			    break
+	import pickle
+	file = open('numpy_image', 'r')
+	im = pickle.load(file)
+	file.close()
+	phasecong(im)