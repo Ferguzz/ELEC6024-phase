f3b409a0abbd5c882b2e27c9450c7d7ede0c0d41
Tom Myers <tom.stephen.myers@gmail.com>
2012-02-15 13:48:51 +0000

    oops deleted most of the files in the last commit.  here they are again

---
+++ c/phase.c
@@ -0,0 +1,20 @@
+int main() {
+	
+	typedef struct {
+		int M;
+		int m;
+		int or;
+		int ft;
+		int pc;
+		int EO;
+		int T;
+	} Results;
+	
+	Results output;
+	
+	output.M = 1;
+	
+	printf("%d\n", output.M);
+	
+	return 0;
+}
