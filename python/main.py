267e81dbb1199cf7448dba5e94ff7eabe43b3e0a
Tom Myers <tom.stephen.myers@gmail.com>
2012-02-15 23:25:42 +0000

    use --contrast 0.x to change input image contrast on the fly.

--- python/main.py
+++ python/main.py
@@ -9,11 +9,13 @@ def processing(input, args):
 	'''DO ALL PROCESSING IN HERE...'''
 	
 	output = collections.namedtuple('Processing', ['phase_out', 'canny'])
-	
-	# Convert to greyscale
-	grey = cv.CreateMat(input.height, input.width, cv.CV_8UC1)
 
-	cv.CvtColor(input, grey, cv.CV_RGB2GRAY)
+	if input.channels != 1:
+		# Convert to greyscale
+		grey = cv.CreateMat(input.height, input.width, cv.CV_8UC1)
+		cv.CvtColor(input, grey, cv.CV_RGB2GRAY)
+	else:
+		grey = input
 	
 	# cv.Smooth(grey, smooth, 19)
 	# smooth = cv.CreateMat(input.height, input.width, cv.CV_8UC1)
@@ -37,20 +39,37 @@ def processing(input, args):
 
 if __name__ == '__main__':
 	
+	# Command line argument parser
 	parser = argparse.ArgumentParser(description = 'Show off phase congruency edge detection.')
-	parser.add_argument('imagename', nargs = '?', default = '../images/baboon.jpg', help = 'Image file.  Default is baboon.jpg')
+	parser.add_argument('imagename', nargs = '?', default = '../images/baboon', help = 'Image file.  Default is baboon.jpg')
 	parser.add_argument('--canny', dest = 'canny', action = 'store_true', help = 'Compare with Canny edge detection operator')
 	parser.add_argument('--corners', dest = 'corners', action = 'store_true', help = 'Display corners detected by phase congruency operator')
 	parser.add_argument('--noise', nargs = '?', metavar = 'int', type = int, dest = 'noise', default = -1, help = 'Phase congruency noise removal method.  -1 = median, - 2 = mode.  Positive values are interpreted as is.')
+	parser.add_argument('--contrast', nargs = '?', metavar = 'float', dest = 'contrast', type = float, default = 1, help = 'Alter contrast of input image')
 	args = parser.parse_args()
 	
 	cv.NamedWindow('Input')
 	
 	if CAM_OR_PIC == 'pic':
 	
-		filename = '../images/' + args.imagename
+		filename = '../images/' + args.imagename + '.jpg'
 		pic = cv.LoadImageM(filename, cv.CV_LOAD_IMAGE_UNCHANGED)
-		cv.ShowImage('Input', pic)
+		
+		if args.contrast != 1:
+			h = cv.CreateMat(pic.height, pic.width, cv.CV_8UC1)
+			s = cv.CreateMat(pic.height, pic.width, cv.CV_8UC1)
+			v = cv.CreateMat(pic.height, pic.width, cv.CV_8UC1)
+			cv.CvtColor(pic, pic, cv.CV_BGR2HSV)
+			cv.Split(pic, h,s,v, None)
+			v = numpy.asarray(v, dtype = float)
+			v *= args.contrast
+			v = numpy.clip(v, 0, 255)
+			v = v.astype('uint8')
+			v = cv.fromarray(v)
+			cv.Merge(h,s,v, None, pic)
+			cv.CvtColor(pic,pic, cv.CV_HSV2BGR)
+		
+		cv.ShowImage('Input', pic)	
 		
  		output = processing(pic, args)
 
